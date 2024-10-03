967beb1214370932e94ac35c0160b3b87a655ad0
Nikita Kharin <harin-1990@yandex.ru>
2024-10-03 22:53:19 +0400

    Юнит 3.2

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -5,10 +5,16 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Никита";
+        const string myName = "Никита";
+        
 
         Console.WriteLine(myName);
+
+        Console.WriteLine("\t Привет, мир");
+        Console.WriteLine("\t Мне 28 лет");
+        Console.WriteLine("\t My name is \n Никита");
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\x23");
 
         Console.ReadKey();
     }
